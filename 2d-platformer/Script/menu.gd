d0d008bf987eaf0db25b7bae9ea0a2511bab2c1c
VeeTheWitchy <125075349+moonsshade@users.noreply.github.com>
2026-05-14 11:59:15 -0700

    lev3

--- 2d-platformer/Script/menu.gd
+++ 2d-platformer/Script/menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 func _on_play_button_pressed() -> void:
 	PlayerStats.score = 0
-	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
+	get_tree().change_scene_to_file("res://Scenes/level_3.tscn")
 
 
 func _on_quit_button_pressed() -> void:
